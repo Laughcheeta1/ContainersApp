610085a45f4104598215a460915ba6490f8303bd
Samuel Corrales <sacosa634@gmail.com>
2023-10-29 11:45:29 -0500

    CRUD comodatos: getCommodatums y getCommodatum

--- src/controllers/commodatum.controller.js
+++ src/controllers/commodatum.controller.js
@@ -1,5 +1,29 @@
 const Commodatum = require("../models/commodatum.model.js");
 
-// const getCommodatum 
+// Obtener un comodato en particular por id (en params)
+
+const getCommodatum = async (req, res) => {
+  try {
+    const foundCommodatum = await Commodatum.findById(req.params.id);
+    if (!foundCommodatum)
+      return res.status(404).json({ message: "Commodatum Not Found" });
+    res.json(foundCommodatum);
+  } catch (error) {
+    return res.status(404).json({ message: "Commodatum Not Found" });
+  }
+};
+
+// Obtener todos los comodatos
+
+const getCommodatums = async (req, res) => {
+  try {
+    const commodatums = await Commodatum.find();
+    res.json(commodatums);
+  } catch (error) {
+    return res.status(404).json({ message: "Commodatums Not Found" });
+  }
+};
+
+// const getCommodatum
 
 // TODO: make the commodatum contoller
--- src/models/commodatum.model.js
+++ src/models/commodatum.model.js
@@ -1,77 +1,75 @@
 const mongoose = require("mongoose");
 
-const receiverSchema = new mongoose.Schema(
-    {
-        id : {
-            type : String,
-            required : true,
-        },
-        name : {
-            type : String,
-            required : true
-        },
-        number : {
-            type : String,
-            required : true
-        }
-    }
-);
+const receiverSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  number: {
+    type: String,
+    required: true,
+  },
+});
 
 const commodatumSchema = new mongoose.Schema(
-    {
-        version : {
-            type : Number
-        },
-        commodatum_id : {
-            type : String,
-            required : true,
-            unique : true,
-            trime : true
-        },
-        container : {
-            type : String,
-            required : true
-        },
-        receiver : {
-            type : receiverSchema,
-            required : true
-        },
-        company : {
-            type : String,
-            required : true
-        },
-        date : {
-            type : Date,
-            required : true
-        },
-        duration : {
-            type : Number,
-            required : true
-        },
-        signature : {
-            type : String,
-            required : true
-        },
-        action : {
-            type : String,
-            required : true
-        },
-        price : {
-            type : Number,
-            required : true
-        },
-        transport_price : {
-            type : Number,
-            required : true
-        },
-        created_by : {
-            type : String,
-            required : true
-        }
+  {
+    version: {
+      type: Number,
     },
-    {
-        timestamps: true
-    }
+    commodatum_id: {
+      type: String,
+      required: true,
+      unique: true,
+      trime: true,
+    },
+    container: {
+      type: String,
+      required: true,
+    },
+    receiver: {
+      type: receiverSchema,
+      required: true,
+    },
+    company: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    signature: {
+      type: String,
+      required: true,
+    },
+    action: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    transport_price: {
+      type: Number,
+      required: true,
+    },
+    created_by: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 module.exports = mongoose.model("Commodatum", commodatumSchema);
