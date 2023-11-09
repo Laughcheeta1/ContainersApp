c04d095839baa40ceca62343c3fb2f552b37dc65
Samuel Corrales <sacosa634@gmail.com>
2023-11-09 17:07:35 -0500

    Algunos avances más de front

---
+++ client/src/components/BarraBusqueda.jsx
@@ -0,0 +1,39 @@
+export default function BarraBusqueda({ metodoBusqueda }) {
+  return (
+    <div className="container-barra-busqueda">
+      <input
+        type="text"
+        className="barra-busqueda"
+        placeholder="Buscar por número"
+        onChange={metodoBusqueda}
+      />
+      <button className="btn btn-verde">
+        Agregar &nbsp;
+        <svg
+          width="22px"
+          height="22px"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            {" "}
+            <path
+              d="M4 12H20M12 4V20"
+              stroke="#FFFFFF"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></path>{" "}
+          </g>
+        </svg>
+      </button>
+    </div>
+  );
+}
--- client/src/pages/ContainersPage.jsx
+++ client/src/pages/ContainersPage.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useContainers } from "../context/ContainerContext";
 import ContainerCard from "../components/ContainerCard";
+import BarraBusqueda from "../components/BarraBusqueda";
+import "../styles/busqueda.css";
 
 export default function ContainersPage() {
   const { getContainers, containers } = useContainers();
@@ -12,10 +14,14 @@ export default function ContainersPage() {
   if (containers.length === 0) return <h1>No Hay Contenedores</h1>;
 
   return (
-    <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-2">
-      {containers.map((container) => (
-        <ContainerCard container={container} key={container._id} />
-      ))}
-    </div>
+    <>
+      <h1 className="search-page-title">Buscar Contenedores</h1>
+      <BarraBusqueda />
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-2">
+        {containers.map((container) => (
+          <ContainerCard container={container} key={container._id} />
+        ))}
+      </div>
+    </>
   );
 }
