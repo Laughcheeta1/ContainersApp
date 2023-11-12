ed1eeeb48e528cec1af3254fe092638d7178bb83
Samuel Corrales <sacosa634@gmail.com>
2023-11-11 19:10:29 -0500

    Añadido botón para cancelar creación de contenedor

--- client/src/pages/ContainerFormPage..jsx
+++ client/src/pages/ContainerFormPage..jsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import { useContainers } from "../context/ContainerContext";
 import { useNavigate, useParams } from "react-router-dom";
 import { containerSchema } from "../schemas/container";
+import { Link } from "react-router-dom";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import "../styles/containerForm.css";
@@ -31,122 +32,134 @@ export default function ContainerFormPage() {
   console.log(errors);
 
   return (
-    <div className="container-form">
-      {containerErrors.map((error, i) => (
-        <div className="container-error" key={i}>
-          {error}
-        </div>
-      ))}
+    <>
+      <div className="container-form">
+        {containerErrors.map((error, i) => (
+          <div className="container-error" key={i}>
+            {error}
+          </div>
+        ))}
 
-      <h2 style={{ fontSize: "32px", fontWeight: 600 }}>
-        Agregar nuevo contenedor.
-      </h2>
+        <h2 style={{ fontSize: "32px", fontWeight: 600 }}>
+          Agregar nuevo contenedor.
+        </h2>
 
-      <hr style={{ marginTop: ".5rem" }} />
+        <hr style={{ marginTop: ".5rem" }} />
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="container-group">
-          <div className="group">
-            <p>{errors.container_id?.message}</p>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="container-group">
+            <div className="group">
+              <p>{errors.container_id?.message}</p>
 
-            <div className="input-group">
-              <label htmlFor="container_id">Número:</label>
-              <input
-                type="text"
-                name="container_id"
-                placeholder="ej: 123456"
-                autoFocus
-                className="input"
-                {...register("container_id", { required: true })}
-              />
+              <div className="input-group">
+                <label htmlFor="container_id">Número:</label>
+                <input
+                  type="text"
+                  name="container_id"
+                  placeholder="ej: 123456"
+                  autoFocus
+                  className="input"
+                  {...register("container_id", { required: true })}
+                />
+              </div>
+            </div>
+
+            <div className="group">
+              <p>{errors.color?.message}</p>
+
+              <div className="input-group">
+                <label htmlFor="color">Color:</label>
+                <input
+                  type="text"
+                  name="color"
+                  placeholder="ej: Verde"
+                  className="input"
+                  {...register("color")}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="container-group">
+            <div className="group">
+              <p>{errors.size?.message}</p>
+
+              <div className="input-group">
+                <label htmlFor="size">Tamaño:</label>
+                <input
+                  name="size"
+                  type="text"
+                  placeholder="ej: Grande"
+                  className="input"
+                  {...register("size")}
+                />
+              </div>
+            </div>
+
+            <div className="group">
+              <p>{errors.qr_code?.message}</p>
+
+              <div className="input-group">
+                <label htmlFor="qr_code">Código QR:</label>
+                <input
+                  name="qr_code"
+                  type="text"
+                  placeholder="QR"
+                  className="input"
+                  {...register("qr_code")}
+                />
+              </div>
             </div>
           </div>
 
           <div className="group">
-            <p>{errors.color?.message}</p>
+            <p>{errors.type?.message}</p>
 
             <div className="input-group">
-              <label htmlFor="color">Color:</label>
+              <label htmlFor="type">Tipo:</label>
               <input
+                name="type"
                 type="text"
-                name="color"
-                placeholder="ej: Verde"
+                placeholder="ej: Laboratorio"
                 className="input"
-                {...register("color")}
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="container-group">
-          <div className="group">
-            <p>{errors.size?.message}</p>
-
-            <div className="input-group">
-              <label htmlFor="size">Tamaño:</label>
-              <input
-                name="size"
-                type="text"
-                placeholder="ej: Grande"
-                className="input"
-                {...register("size")}
+                {...register("type")}
               />
             </div>
           </div>
 
-          <div className="group">
-            <p>{errors.qr_code?.message}</p>
+          <label
+            style={{ marginTop: "1rem", display: "inline-flex" }}
+            htmlFor="notes"
+          >
+            Descripción:
+          </label>
+          <textarea
+            name="notes"
+            rows="3"
+            placeholder="ej: Laboratorio de química para universidad"
+            className="input"
+            {...register("notes")}
+          ></textarea>
 
-            <div className="input-group">
-              <label htmlFor="qr_code">Código QR:</label>
-              <input
-                name="qr_code"
-                type="text"
-                placeholder="QR"
-                className="input"
-                {...register("qr_code")}
-              />
-            </div>
+          <div style={{ marginTop: "1rem" }} className="container-group">
+            <button
+              style={{ width: "100%" }}
+              className="btn btn-verde"
+              type="submit"
+            >
+              Guardar contenedor
+            </button>
+
+            <Link
+              style={{ width: "100%" }}
+              className="btn btn-gris"
+              to="/containers"
+            >
+              Volver
+            </Link>
           </div>
-        </div>
-
-        <div className="group">
-          <p>{errors.type?.message}</p>
-
-          <div className="input-group">
-            <label htmlFor="type">Tipo:</label>
-            <input
-              name="type"
-              type="text"
-              placeholder="ej: Laboratorio"
-              className="input"
-              {...register("type")}
-            />
-          </div>
-        </div>
-
-        <label
-          style={{ marginTop: "1rem", display: "inline-flex" }}
-          htmlFor="notes"
-        >
-          Descripción:
-        </label>
-        <textarea
-          name="notes"
-          rows="3"
-          placeholder="ej: Laboratorio de química para universidad"
-          className="input"
-          {...register("notes")}
-        ></textarea>
-
-        <button
-          style={{ marginTop: ".8rem", width: "100%" }}
-          className="btn btn-verde"
-          type="submit"
-        >
-          Guardar contenedor
-        </button>
-      </form>
-    </div>
+        </form>
+      </div>
+    </>
   );
 }
