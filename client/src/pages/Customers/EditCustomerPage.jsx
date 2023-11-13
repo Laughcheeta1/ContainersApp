20c64ebb41dd5786e8d4f017e2f1d97fbba24b9a
Samuel Corrales <sacosa634@gmail.com>
2023-11-13 18:15:59 -0500

    Corregida ortografía de formulario para editar cliente

--- client/src/pages/Customers/EditCustomerPage.jsx
+++ client/src/pages/Customers/EditCustomerPage.jsx
@@ -9,113 +9,110 @@ import AlertDialogCrear from "../../components/AlertDialogCrear";
 
 import "../../styles/formPage.css";
 
-export default function EditCustomerPage()
-{
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm({
-        resolver: zodResolver(editCustomerSchema)
-    });
+export default function EditCustomerPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(editCustomerSchema),
+  });
 
-    const { editCustomer, errors: customerErrors } = useCustomers();
-    const navigate = useNavigate();
+  const { editCustomer, errors: customerErrors } = useCustomers();
+  const navigate = useNavigate();
 
-    const [wasSubmitted, setWasSubmitted] = useState(false);
+  const [wasSubmitted, setWasSubmitted] = useState(false);
 
-    const onSubmit = async (data) => {
-        await editCustomer(customer._id, data);
-        setWasSubmitted(true);
-    };
+  const onSubmit = async (data) => {
+    await editCustomer(customer._id, data);
+    setWasSubmitted(true);
+  };
 
-    useEffect(() => {
-        if (wasSubmitted && customerErrors.length === 0)
-            return navigate("/customers");
-        setWasSubmitted(false);
-    }, [customerErrors, wasSubmitted]);
+  useEffect(() => {
+    if (wasSubmitted && customerErrors.length === 0)
+      return navigate("/customers");
+    setWasSubmitted(false);
+  }, [customerErrors, wasSubmitted]);
 
-    return (
-        <>
-            <div className="container-form">
-                {customerErrors.map((error, i) => (
-                    <div className="container-error" key={i}>
-                        {error}
-                    </div>
-                ))}
+  return (
+    <>
+      <div className="container-form">
+        {customerErrors.map((error, i) => (
+          <div className="container-error" key={i}>
+            {error}
+          </div>
+        ))}
 
-                <h2 style={{ fontSize: "32px", fontWeight: 600 }}>
-                    Editar Cliente
-                </h2>
+        <h2 style={{ fontSize: "32px", fontWeight: 600 }}>Editar Cliente</h2>
 
-                <hr style={{ marginTop: ".5rem" }} />
+        <hr style={{ marginTop: ".5rem" }} />
 
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="container-group">
-                        <div className="group">
-                            <p>{errors.nameT?.message}</p>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="container-group">
+            <div className="group">
+              <p>{errors.nameT?.message}</p>
 
-                            <div className="input-group">
-                                <label htmlFor="type">Nombre:</label>
-                                <input
-                                name="name"
-                                type="text"
-                                className="input"
-                                {...register("name")}
-                                />
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="container-group">
-                        <div className="group">
-                            <p>{errors.phone?.message}</p>
-
-                            <div className="input-group">
-                                <label htmlFor="type">Telefono:</label>
-                                <input
-                                name="phone"
-                                type="text"
-                                className="input"
-                                {...register("phone")}
-                                />
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="container-group">
-                        <div className="group">
-                            <p>{errors.address?.message}</p>
-
-                            <div className="input-group">
-                                <label htmlFor="type">Direccion:</label>
-                                <input
-                                name="address"
-                                type="text"
-                                className="input"
-                                {...register("address")}
-                                />
-                            </div>
-                        </div>
-                    </div>
-
-                    <div style={{ marginTop: "1rem" }} className="container-group">
-                        <AlertDialogCrear
-                        buttonMessage="Guardar cambios"
-                        descriptionMessage="Se guardaran los cambios que entraste"
-                        onSubmit={handleSubmit(onSubmit)}
-                        />
-
-                        <Link
-                        style={{ width: "100%" }}
-                        className="btn btn-gris"
-                        to="/customers"
-                        >
-                        Cancelar
-                        </Link>
-                    </div>
-                </form>
+              <div className="input-group">
+                <label htmlFor="type">Nombre:</label>
+                <input
+                  name="name"
+                  type="text"
+                  className="input"
+                  {...register("name")}
+                />
+              </div>
             </div>
-        </>
-    )
+          </div>
+
+          <div className="container-group">
+            <div className="group">
+              <p>{errors.phone?.message}</p>
+
+              <div className="input-group">
+                <label htmlFor="type">Teléfono:</label>
+                <input
+                  name="phone"
+                  type="text"
+                  className="input"
+                  {...register("phone")}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="container-group">
+            <div className="group">
+              <p>{errors.address?.message}</p>
+
+              <div className="input-group">
+                <label htmlFor="type">Dirección:</label>
+                <input
+                  name="address"
+                  type="text"
+                  className="input"
+                  {...register("address")}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div style={{ marginTop: "1rem" }} className="container-group">
+            <AlertDialogCrear
+              buttonMessage="Guardar cambios"
+              descriptionMessage="Se guardaran los cambios que entraste"
+              onSubmit={handleSubmit(onSubmit)}
+            />
+
+            <Link
+              style={{ width: "100%" }}
+              className="btn btn-gris"
+              to="/customers"
+            >
+              Cancelar
+            </Link>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
