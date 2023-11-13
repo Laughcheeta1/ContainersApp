bb27204f5ea4fb6b3d93ddf703441286d1133219
Samuel Corrales <sacosa634@gmail.com>
2023-11-13 13:27:35 -0500

    Corregido mensaje de error para crear cliente

--- client/src/schemas/customer.js
+++ client/src/schemas/customer.js
@@ -4,7 +4,7 @@ export const customerSchema = z.object({
   version: z.number().default(1),
 
   company_NIT: z.string().length(9, {
-    message: "El NIT debe tener al menos 9 digitos",
+    message: "El NIT debe tener exactamente 9 digitos",
   }),
 
   name: z.string().min(1, {
