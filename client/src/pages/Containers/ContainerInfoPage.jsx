8719b4a7c392a21431dd266131d4a22aba268e04
Samuel Corrales <sacosa634@gmail.com>
2023-11-13 17:39:35 -0500

    Añadida la págína de un contenedor específico

--- client/src/pages/Containers/ContainerInfoPage.jsx
+++ client/src/pages/Containers/ContainerInfoPage.jsx
@@ -3,6 +3,7 @@ import { useContainers } from "../../context/ContainerContext";
 import { useParams } from "react-router-dom";
 import LoadingScreen from "../../components/LoadingScreen";
 import "../../styles/infoPage.css";
+import containerDummy from "../../assets/containerDummy.png";
 
 export default function ContainerInfoPage() {
   const params = useParams();
@@ -26,20 +27,82 @@ export default function ContainerInfoPage() {
   return (
     <div>
       {container ? (
-        <div className="title">
-          Contenedor no.{" "}
-          <span
-            style={{
-              fontSize: "24px",
-              fontWeight: "400",
-              marginInline: "1rem",
-            }}
-          >
-            {container.container_id}
-          </span>
-          {" · "}
-          {container.type}
-        </div>
+        <>
+          <div className="title">
+            Contenedor no.{" "}
+            <span
+              style={{
+                fontSize: "24px",
+                fontWeight: "400",
+                marginInline: "1rem",
+              }}
+            >
+              {container.container_id}
+            </span>
+            {" · "}
+            {container.type}
+          </div>
+
+          <div className="container-info">
+            <h1 className="container-title-info">Información.</h1>
+            <div className="info">
+              <div style={{ display: "flex", flexDirection: "column" }}>
+                <div className="container">
+                  <label
+                    style={{
+                      fontSize: "20px",
+                      marginBottom: "1rem",
+                      fontWeight: "500",
+                    }}
+                    htmlFor="image"
+                  >
+                    Foto contenedor.
+                  </label>
+                  <img
+                    name="image"
+                    style={{
+                      height: "18rem",
+                      width: "auto",
+                      objectFit: "cover",
+                    }}
+                    src={containerDummy}
+                    alt=""
+                  />
+                </div>
+              </div>
+
+              <div className="info-column">
+                <div className="details container">
+                  <div className="info-row">
+                    <p className="text">
+                      Color: <span>{container.color}</span>
+                    </p>
+
+                    <p className="text">
+                      Tipo: <span>{container.type}</span>
+                    </p>
+
+                    <p className="text">
+                      Tamaño: <span>{container.size}</span>
+                    </p>
+
+                    <p className="text">
+                      Estado: <span>{container.status}</span>
+                    </p>
+                  </div>
+                </div>
+
+                <div className="container">
+                  <div className="info-row">
+                    <p className="text">
+                      Notas: <span>{container.notes}</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
       ) : (
         <LoadingScreen />
       )}
