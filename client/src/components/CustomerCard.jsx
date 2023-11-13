d2bbdbeb37d9929b17e3321e3b53f6dd074d3691
Samuel Corrales <sacosa634@gmail.com>
2023-11-12 20:57:37 -0500

    Cambios en CustomerCard y CustomerPage

--- client/src/components/CustomerCard.jsx
+++ client/src/components/CustomerCard.jsx
@@ -3,32 +3,35 @@ import utc from "dayjs/plugin/utc";
 dayjs.extend(utc);
 
 export default function CustomerCard({ customer }) {
-    return (
-        <div className="resultado-busqueda">
-            <div className="flex gap-x-2 items-center">
-                <p style={{ marginRight: "1rem" }} className="text-xl font-medium">
-                    {customer.name}
-                </p>
-                    <button
-                    className="btn btn-rojo"
-                    onClick={() => {
-                    deleteContainer(container._id);
-                    }} >
-                    Eliminar
-                </button>
-            </div>
-            <p style={{ marginRight: "1rem" }} className="text-xl font-medium">
-                {customer.phone}
-            </p>
-            <p style={{ marginRight: "1rem" }} className="text-xl font-medium">
-                {customer.address}
-            </p>
-            <p style={{ marginRight: "1rem" }} className="text-xl font-medium">
-                {customer.company_NIT}
-            </p>
-            <p className="text-sm">
-                {dayjs(customer.updatedAt).utc().format("DD/MM/YY")}
-            </p>
-        </div>
-    );
+  return (
+    <div className="resultado-busqueda">
+      <div className="flex gap-x-2 items-center">
+        <p style={{ marginRight: "1rem" }} className="text-xl font-medium">
+          {customer.name}
+        </p>
+        <button
+          className="btn btn-rojo"
+          onClick={() => {
+            deleteContainer(container._id);
+          }}
+        >
+          Eliminar
+        </button>
+      </div>
+      <p style={{ marginRight: "1rem" }} className="text-xl font-medium">
+        {customer.phone}
+      </p>
+      <p style={{ marginRight: "1rem" }} className="text-xl font-medium">
+        {customer.address}
+      </p>
+      <p style={{ marginRight: "1rem" }} className="text-xl font-medium">
+        <span>NIT: </span>
+        {customer.company_NIT}
+      </p>
+      <p className="text-xl font-medium">
+        <span>Últ mod: </span>
+        {dayjs(customer.updatedAt).utc().format("DD/MM/YY")}
+      </p>
+    </div>
+  );
 }
