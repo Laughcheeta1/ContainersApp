e5a7251731610e4ed7e816d3cee1c7ee25edba8d
Samuel Corrales <sacosa634@gmail.com>
2023-11-17 10:45:07 -0500

    Corregido input para número de unidades en editar ítem

--- client/src/pages/Items/EditItemPage.jsx
+++ client/src/pages/Items/EditItemPage.jsx
@@ -11,150 +11,158 @@ import LoadingScreen from "../../components/LoadingScreen";
 import "../../styles/formPage.css";
 
 export default function EditCustomerPage() {
-    const {
-      register,
-      handleSubmit,
-      formState: { errors },
-    } = useForm({ resolver: zodResolver(itemSchema) });
-  
-    const { updateItem, errors: itemsErrors, getItem } = useItems();
-    const navigate = useNavigate();
-  
-    const [wasSubmitted, setWasSubmitted] = useState(false);
-    const [item, setItem] = useState(null);
-    const params = useParams();
-  
-    useEffect(() => {
-      try {
-        const loadItem = async () => {
-          const customerInfo = await getItem(params.id);
-          setItem(() => customerInfo);
-        };
-  
-        loadItem();
-      } catch (error) {
-        console.log(error);
-      }
-    }, []);
-  
-    const onSubmit = async (data) => {
-      await updateItem(params.id, data);
-      setWasSubmitted(true);
-    };
-  
-    useEffect(() => {
-      if (wasSubmitted && itemsErrors.length === 0)
-        return navigate("/items");
-      setWasSubmitted(false);
-    }, [itemsErrors, wasSubmitted]);
-  
-    return (
-      <>
-        {item ? (
-          <div className="container-form">
-            {itemsErrors.map((error, i) => (
-              <div className="container-error" key={i}>
-                {error}
-              </div>
-            ))}
-  
-            <h2 style={{ fontSize: "32px", fontWeight: 600 }}>
-              Editar Item{" "}
-              <span style={{ fontSize: "22px", fontWeight: "500" }}>
-                Nombre: {item.name}
-              </span>
-            </h2>
-  
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="container-group">
-                <div className="group">
-                  <p>{errors.name?.message}</p>
-  
-                  <div className="input-group">
-                    <label htmlFor="name">Nombre:</label>
-                    <input 
-                      name="name"
-                      type="text"
-                      defaultValue={item.name}
-                      className="input"
-                      {...register("name")}
-                    />
-                  </div>
-                </div>
-              </div>
-  
-              <div className="container-group">
-                <div className="group">
-                  <p>{errors.brand?.message}</p>
-  
-                  <div className="input-group">
-                    <label htmlFor="brand">Marca:</label>
-                    <input
-                      name="brand"
-                      type="text"
-                      defaultValue={item.brand}
-                      className="input"
-                      {...register("brand")}
-                    />
-                  </div>
-                </div>
-              </div>
-  
-              <div className="container-group">
-                <div className="group">
-                  <p>{errors.total_quantity?.message}</p>
-  
-                  <div className="input-group">
-                    <label htmlFor="total_quantity">Cantidad Total:</label>
-                    <input
-                      name="total_quantity"
-                      type="text"
-                      defaultValue={item.total_quantity}
-                      className="input"
-                      {...register("total_quantity")}
-                    />
-                  </div>
-                </div>
-              </div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(itemSchema) });
 
-              <div className="container-group">
-                <div className="group">
-                  <p>{errors.available_quantity?.message}</p>
-  
-                  <div className="input-group">
-                    <label htmlFor="available_quantity">Cantidad Disponible:</label>
-                    <input
-                      name="available_quantity"
-                      type="text"
-                      defaultValue={item.available_quantity}
-                      className="input"
-                      {...register("available_quantity")}
-                    />
-                  </div>
+  const { updateItem, errors: itemsErrors, getItem } = useItems();
+  const navigate = useNavigate();
+
+  const [wasSubmitted, setWasSubmitted] = useState(false);
+  const [item, setItem] = useState(null);
+  const params = useParams();
+
+  useEffect(() => {
+    try {
+      const loadItem = async () => {
+        const customerInfo = await getItem(params.id);
+        setItem(() => customerInfo);
+      };
+
+      loadItem();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const onSubmit = async (data) => {
+    await updateItem(params.id, data);
+    setWasSubmitted(true);
+  };
+
+  useEffect(() => {
+    if (wasSubmitted && itemsErrors.length === 0) return navigate("/items");
+    setWasSubmitted(false);
+  }, [itemsErrors, wasSubmitted]);
+
+  return (
+    <>
+      {item ? (
+        <div className="container-form">
+          {itemsErrors.map((error, i) => (
+            <div className="container-error" key={i}>
+              {error}
+            </div>
+          ))}
+
+          <h2
+            style={{ fontSize: "32px", fontWeight: 600, marginBottom: "2rem" }}
+          >
+            Editar Item{" "}
+            <span style={{ fontSize: "22px", fontWeight: "500" }}>
+              Nombre: {item.name}
+            </span>
+          </h2>
+
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="container-group">
+              <div className="group">
+                {errors.name?.message ? <p>{errors.name?.message}</p> : null}
+
+                <div className="input-group">
+                  <label htmlFor="name">Nombre:</label>
+                  <input
+                    name="name"
+                    type="text"
+                    defaultValue={item.name}
+                    className="input"
+                    {...register("name")}
+                  />
                 </div>
               </div>
-  
-              <div style={{ marginTop: "2rem" }} className="container-group">
-                <AlertDialogCrear
-                  buttonMessage="Guardar cambios"
-                  descriptionMessage="Se guardaran los cambios que entraste"
-                  onSubmit={handleSubmit(onSubmit)}
-                />
-  
-                <Link
-                  style={{ width: "100%" }}
-                  className="btn btn-gris"
-                  to="/items"
-                >
-                  Cancelar
-                </Link>
+            </div>
+
+            <div className="container-group">
+              <div className="group">
+                {errors.brand?.message ? <p>{errors.brand?.message}</p> : null}
+
+                <div className="input-group">
+                  <label htmlFor="brand">Marca:</label>
+                  <input
+                    name="brand"
+                    type="text"
+                    defaultValue={item.brand}
+                    className="input"
+                    {...register("brand")}
+                  />
+                </div>
               </div>
-            </form>
-          </div>
-        ) : (
-          <LoadingScreen />
-        )}
-      </>
-    );
-  }
-  
+            </div>
+
+            <div className="container-group">
+              <div className="group">
+                {errors.total_quantity?.message ? (
+                  <p>{errors.total_quantity?.message}</p>
+                ) : null}
+
+                <div className="input-group">
+                  <label htmlFor="total_quantity">Cantidad Total:</label>
+                  <input
+                    name="total_quantity"
+                    type="number"
+                    min={0}
+                    defaultValue={item.total_quantity}
+                    className="input"
+                    {...register("total_quantity")}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="container-group">
+              <div className="group">
+                {errors.available_quantity?.message ? (
+                  <p>{errors.available_quantity?.message}</p>
+                ) : null}
+
+                <div className="input-group">
+                  <label htmlFor="available_quantity">
+                    Cantidad Disponible:
+                  </label>
+                  <input
+                    name="available_quantity"
+                    type="number"
+                    min={0}
+                    defaultValue={item.available_quantity}
+                    className="input"
+                    {...register("available_quantity")}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div style={{ marginTop: "1.5rem" }} className="container-group">
+              <AlertDialogCrear
+                buttonMessage="Guardar cambios"
+                descriptionMessage="Se guardaran los cambios que entraste"
+                onSubmit={handleSubmit(onSubmit)}
+              />
+
+              <Link
+                style={{ width: "100%" }}
+                className="btn btn-gris"
+                to="/items"
+              >
+                Cancelar
+              </Link>
+            </div>
+          </form>
+        </div>
+      ) : (
+        <LoadingScreen />
+      )}
+    </>
+  );
+}
